9178aa2a59da0d32294ea75765e55f018d3bf986
guozengxin <guozengxin@bytedance.com>
2016-03-08 19:45:01 +0800

    Add 4 tools: cgrep gogrep jgrep pygrep

--- install.sh
+++ install.sh
@@ -5,3 +5,9 @@ list="gitconfig vimrc vim"
 for l in $list; do
 	ln -sf $now/$l ~/.${l}
 done
+
+mkdir -p ~/bin/
+list="jgrep pygrep cgrep gogrep"
+for l in $list; do
+	ln -sf $now/$l ~/bin/${l}
+done
