21a8040b60dd7f8a288d3bda6d2cbd7a2e0fbd8e
guozengxin <guozengxin@bytedance.com>
2016-08-11 22:21:14 +0800

    update some code

--- install.sh
+++ install.sh
@@ -11,3 +11,5 @@ list="jgrep pygrep cgrep gogrep bt pstop"
 for l in $list; do
 	ln -sf $now/bin/$l ~/bin/${l}
 done
+git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+sudo apt-get -y install exuberant-ctags
